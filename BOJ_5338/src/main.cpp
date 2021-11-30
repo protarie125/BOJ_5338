b1c74b95e6d09ebd571420c1f53465be919cd85d
protarie125 <proarie@gmail.com>
2021-12-01 07:33:59 +0900

    초기 커밋.

---
+++ BOJ_5338/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    cout << R"(       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|)";
+
+    return 0;
+}
